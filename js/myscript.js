dff0e930df51e8bd3d0bf4470110e5954d13f410
Pavel Usov <usov.p@mail.ru>
2020-10-04 12:10:39 +1000

    add myscript.js

---
+++ js/myscript.js
@@ -0,0 +1,19 @@
+window.onload = function() {
+	var search = document.querySelector('.search-ic img');
+	var form = document.querySelector('.icons form');
+	var input = document.querySelector('.icons form input');
+	var clipb = document.querySelector('.clipb');
+	var body = document.querySelector('body');
+	search.onclick = function() {
+		form.style.display = 'block';
+		search.style.display = 'none';	
+		clipb.style.display = "none";
+	};
+	body.onclick = function(e) {
+		if(e.target != input & e.target != search) {
+			form.style.display = 'none';
+			search.style.display = 'block';
+			clipb.style.display = 'block';
+		};	
+	};
+};
